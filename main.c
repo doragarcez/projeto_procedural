81e15ae12c779b8daf11df67d483e1368847a584
doragarcez <garcezalves.isadora@gmail.com>
2026-09-04 21:16:45 -0300

    feat: add funções do desafio adicional

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #define MAX_PRODUTOS 100
+#define ESTOQUE_MINIMO 5
 
 int menu () {
     int opcao;
@@ -12,6 +13,10 @@ int menu () {
     printf("4. Entrada de estoque\n");
     printf("5. Saida de estoque\n");
     printf("6. Mostrar valor total do estoque\n");
+    printf("7. Mostrar produto com maior quantidade\n");
+    printf("8. Mostrar produto com menor quantidade\n");
+    printf("9. Mostrar produto mais caro\n");
+    printf("10. Listagem de produtos com estoque baixo\n");
     printf("0. Sair\n");
     printf("Escolha uma opcao: ");
     scanf("%d", &opcao);
@@ -129,12 +134,89 @@ int saida_estoque(Produto *produtos, int quantidade_produtos) {
 }
 
 void mostrar_valor_total_estoque(Produto *produtos, int quantidade_produtos) {
+    printf("===== VALOR TOTAL DO ESTOQUE =====\n");
+    if (quantidade_produtos == 0) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
     float valor_total = 0.0;
     for (int i = 0; i < quantidade_produtos; i++) {
-        valor_total += produtos[i].preco_unitario * produtos[i].quantidade;
+        float valor_produto = produtos[i].preco_unitario * produtos[i].quantidade;
+        printf("Produto: %s - Valor em estoque: %.2f\n", produtos[i].nome, valor_produto);
+        valor_total += valor_produto;
     }
-    printf("===== VALOR TOTAL DO ESTOQUE =====\n");
     printf("Valor total do estoque: %.2f\n", valor_total);
+}
+
+void mostrar_produto_maior_quantidade(Produto *produtos, int quantidade_produtos) {
+    printf("===== PRODUTO COM MAIOR QUANTIDADE =====\n");
+    if (quantidade_produtos == 0) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    int indice_maior = 0;
+    for (int i = 1; i < quantidade_produtos; i++) {
+        if (produtos[i].quantidade > produtos[indice_maior].quantidade) {
+            indice_maior = i;
+        }
+    }
+    printf("Nome: %s", produtos[indice_maior].nome);
+    printf("Quantidade em estoque: %d\n", produtos[indice_maior].quantidade);
+}
+
+void mostrar_produto_menor_quantidade(Produto *produtos, int quantidade_produtos) {
+    printf("===== PRODUTO COM MENOR QUANTIDADE =====\n");
+    if (quantidade_produtos == 0) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    int indice_menor = 0;
+    for (int i = 1; i < quantidade_produtos; i++) {
+        if (produtos[i].quantidade < produtos[indice_menor].quantidade) {
+            indice_menor = i;
+        }
+    }
+    printf("Nome: %s", produtos[indice_menor].nome);
+    printf("Quantidade em estoque: %d\n", produtos[indice_menor].quantidade);
+}
+
+void mostrar_produto_mais_caro(Produto *produtos, int quantidade_produtos) {
+    printf("===== PRODUTO MAIS CARO =====\n");
+    if (quantidade_produtos == 0) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    int indice_mais_caro = 0;
+    for (int i = 1; i < quantidade_produtos; i++) {
+        if (produtos[i].preco_unitario > produtos[indice_mais_caro].preco_unitario) {
+            indice_mais_caro = i;
+        }
+    }
+    printf("Nome: %s", produtos[indice_mais_caro].nome);
+    printf("Preco unitario: %.2f\n", produtos[indice_mais_caro].preco_unitario);
+}
+
+void listar_produtos_estoque_baixo(Produto *produtos, int quantidade_produtos) {
+    printf("===== PRODUTOS COM ESTOQUE BAIXO =====\n");
+    if (quantidade_produtos == 0) {
+        printf("Nenhum produto cadastrado.\n");
+        return;
+    }
+
+    int encontrou = 0;
+    for (int i = 0; i < quantidade_produtos; i++) {
+        if (produtos[i].quantidade < ESTOQUE_MINIMO) {
+            printf("Nome: %s", produtos[i].nome);
+            printf("Quantidade em estoque: %d\n", produtos[i].quantidade);
+            encontrou = 1;
+        }
+    }
+    if (!encontrou) {
+        printf("Nenhum produto com estoque baixo.\n");
+    }
 }
 
 int main() {
@@ -161,6 +243,18 @@ int main() {
                 break;
             case 6:
                 mostrar_valor_total_estoque(produtos, quantidade_produtos);
+                break;
+            case 7:
+                mostrar_produto_maior_quantidade(produtos, quantidade_produtos);
+                break;
+            case 8:
+                mostrar_produto_menor_quantidade(produtos, quantidade_produtos);
+                break;
+            case 9:
+                mostrar_produto_mais_caro(produtos, quantidade_produtos);
+                break;
+            case 10:
+                listar_produtos_estoque_baixo(produtos, quantidade_produtos);
                 break;
             case 0:
                 printf("Saindo do sistema...\n");
